80a3d5718fceaa53723cc8f947024ccb700959ed
pietro573 <pietro.czerski@escola.pr.gov.br>
2026-05-27 08:58:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// npm init
+// npm i express
+// RAPIDAPI CLIENT
+
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json())
+const fs = require('fs')
+
+app.get('/ola', (req, res)=>{
+    res.json('hello class')
+})
+
+app.post("/clientes/cadastro", (req,res) => {
+    const cliente = req.body
+    if (!cliente || Object.keys(cliente).length === 0) {
+        res.status(400).json({resposta: "Body não preenchido"})
+    } else {
+        try {
+            const bd = JSON.parse(fs.readFileSync('bd.json', 'utf8'))
+            bd.push(cliente)
+            fs.writeFileSync('bd.json', JSON.stringify(bd), 'utf8')
+            res.status(200).json({resposta: "Cliente cadastrado com sucesso!"})
+        } catch(error) {
+            res.status(500).json({resposta: error.message})
+        }
+    }    
+})
+
+
+app.listen(port, ()=> {
+    console.log('API executado com sucesso')
+})
